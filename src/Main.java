12dcac1ea3ef84d8cd69b57e12aa7b59cb2d4a0d
João <joaopaulokimi@gmail.com>
2024-08-20 14:22:36 -0300

    add: herança e polimorfismo e classes de domínio

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.joao.desafio.dominio.Bootcamp;
 import br.com.joao.desafio.dominio.Curso;
+import br.com.joao.desafio.dominio.Dev;
 import br.com.joao.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -8,12 +10,12 @@ public class Main {
         Curso curso = new Curso();
 
         Curso curso1 = new Curso();
-        curso1.setTitulo("curso java");
+        curso1.setTitulo("java");
         curso1.setDescricao("descrição curso java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso2.setTitulo("curso js");
+        curso2.setTitulo("js");
         curso2.setDescricao("descrição curso js");
         curso2.setCargaHoraria(4);
 
@@ -22,10 +24,43 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+/*        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+                                        */
 
+        Bootcamp bootcampClaro = new Bootcamp();
+        bootcampClaro.setNome("Bootcamp Java Developer");
+        bootcampClaro.setDescricao("Descrição Bootcamp Claro");
+        bootcampClaro.getConteudos().add(curso1);
+        bootcampClaro.getConteudos().add(curso2);
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcampClaro);
+        System.out.println("Conteúdos Inscritos" + devJoao.getConteudosIncritos());
+
+        devJoao.progredir();
+        System.out.println("--");
+
+        System.out.println("Conteúdos Inscritos" + devJoao.getConteudosIncritos());
+        System.out.println("Conteúdos Concluídos" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcualarTotalXp());
+
+
+        System.out.println("---------------------------");
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcampClaro);
+        System.out.println("Conteúdos Inscritos" + devCamila.getConteudosIncritos());
+
+        devCamila.progredir();
+        System.out.println("--");
+
+        System.out.println("Conteúdos Inscritos" + devCamila.getConteudosIncritos());
+        System.out.println("Conteúdos Concluídos" + devCamila.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcualarTotalXp());
 
     }
 }
--- src/br/com/joao/desafio/dominio/Mentoria.java
+++ src/br/com/joao/desafio/dominio/Mentoria.java
@@ -2,9 +2,9 @@ package br.com.joao.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo {
+
+    private int cargaHoraria;
     private LocalDate data;
 
     public Mentoria() {
@@ -13,26 +13,23 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
     }
 
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public int getCargaHoraria() {
+        return cargaHoraria;
     }
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
     }
 
     public LocalDate getData() {
